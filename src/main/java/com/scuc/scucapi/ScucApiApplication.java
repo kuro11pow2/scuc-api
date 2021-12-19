0ce7425fa5c0adec539345bac41f1aa0ead8ab3e
kuro11pow2 <kuro11pow2@gmail.com>
2021-12-20 06:46:06 +0900

    프로젝트 재설정

--- src/main/java/com/scuc/scucapi/ScucApiApplication.java
+++ src/main/java/com/scuc/scucapi/ScucApiApplication.java
@@ -2,10 +2,8 @@ package com.scuc.scucapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = { "classpath:secret.properties" })
 public class ScucApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/scuc/scucapi/repository/UniversityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.scuc.scucapi.repository;
-
-import com.scuc.scucapi.entity.University;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UniversityRepository extends JpaRepository<University, Long> {
-}
