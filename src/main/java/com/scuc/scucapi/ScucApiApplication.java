bda1402efcee066f8f0471af09f305fc11102d03
kuro11pow2 <kuro11pow2@gmail.com>
2021-12-20 23:00:58 +0900

    의존성 추가, 민감 정보 숨김 및 기타 설정

--- src/main/java/com/scuc/scucapi/ScucApiApplication.java
+++ src/main/java/com/scuc/scucapi/ScucApiApplication.java
@@ -2,8 +2,12 @@ package com.scuc.scucapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@PropertySource(value = { "classpath:secret.properties" })
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScucApiApplication {
 
     public static void main(String[] args) {
